73594a1b8941d8b9913a1ced44a11b20f5bb61a0
viveknhc <viveknhc@gmail.com>
2023-09-21 18:18:40 +0530

    21/09/23

--- erp.php
+++ erp.php
@@ -1,35 +1,90 @@
 <?php include 'partials/header.php';?>
 <header>
     <div class="erp-banner relative-box">
-        <div class="row">
-            <div class="col-lg-6">
-                <div>
-                    <h1>
-                        ENTERPRISE <br> RESOURSE <br>
-                        PLANNING
-                    </h1>
-                    <p>
-                        Injected humour, or randomised words which don't look even slightly believable. If you are going to use
-                        a passage.
-                    </p>
-                    <button class="primary-btn">read more</button>
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-6 left-side">
+                    <div class="px-2">
+                        <img src="assets/images/erp/digital-campus-logo.png" alt="">
+                        <h1>
+                            <!-- Digital <br> Campus <br>  -->
+                            <span class="mt-5">Digital Campus <br>ERP </span>
+                        </h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere quis deserunt repellat
+                            corporis mollitia</p>
+                        <button class="primary-btn">View More</button>
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-6">
-                <img class="img-fluid banner-img2" src="assets/images/erp/banner-img2.png" alt="">
-                <img class="img-fluid banner-img1" src="assets/images/erp/banner-img1.png" alt="">
+
+                <!-- <div class="col-lg-6">
+                    <img class="img-fluid banner-img2" src="assets/images/erp/banner-img2.png" alt="">
+                    <img class="img-fluid banner-img1" src="assets/images/erp/banner-img1.png" alt="">
+                </div> -->
             </div>
         </div>
-
     </div>
 </header>
 
 <section>
     <div class="container">
-        <h5 class="section-top-head">Best ERP Systems</h5>
-        <h2 class="section-head">What are ERP Systems?</h2>
-        <div class="row">
+        <div class="text-center">
+            <h5 class="section-top-head">Complete</h5>
+            <h2 class="section-head">Student Cycle</h2>
+            <p>With end to end processes right from enquiry to alumini,tracking and tracing complete trail of student
+                activity has never been so quick,easy and intuitive</p>
+        </div>
 
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="text-center">
+                    <img src="assets/images/erp/addmission.png" alt="">
+                    <p>Admission</p>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="text-center">
+                    <img src="assets/images/erp/id-card.png" alt="">
+                    <p>ID Card</p>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="text-center">
+                    <img src="assets/images/erp/attendance.png" alt="">
+                    <p>Attendance</p>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="text-center">
+                    <img src="assets/images/erp/grade-book.png" alt="">
+                    <p>Grade Book</p>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="text-center">
+                    <img src="assets/images/erp/result-processing.png" alt="">
+                    <p>Result Processing</p>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="text-center">
+                    <img src="assets/images/erp/discipline.png" alt="">
+                    <p>Discipline</p>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="text-center">
+                    <img src="assets/images/erp/nursing.png" alt="">
+                    <p>Nursing</p>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="text-center">
+                    <img src="assets/images/erp/certificate.png" alt="">
+                    <p>Certificate</p>
+                </div>
+            </div>
         </div>
     </div>
 </section>
