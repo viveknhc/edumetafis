58a8720b37f335573a4d2841cded2fbeb775f770
viveknhc <viveknhc@gmail.com>
2023-09-22 18:36:07 +0530

    22/09/23

--- erp.php
+++ erp.php
@@ -26,67 +26,159 @@
     </div>
 </header>
 
-<section>
+<section class="student-cycle">
     <div class="container">
-        <div class="text-center">
-            <h5 class="section-top-head">Complete</h5>
-            <h2 class="section-head">Student Cycle</h2>
-            <p>With end to end processes right from enquiry to alumini,tracking and tracing complete trail of student
-                activity has never been so quick,easy and intuitive</p>
-        </div>
-
         <div class="row">
-            <div class="col-lg-3">
-                <div class="text-center">
-                    <img src="assets/images/erp/addmission.png" alt="">
-                    <p>Admission</p>
+            <div class="col-lg-6">
+                <div class="left relative-box">
+                    <img class="img-fluid left-img-1" src="assets/images/erp/left1.png" alt="">
+                    <img class="img-fluid left-img-2" src="assets/images/erp/left2.png" alt="">
                 </div>
             </div>
-
-            <div class="col-lg-3">
-                <div class="text-center">
-                    <img src="assets/images/erp/id-card.png" alt="">
-                    <p>ID Card</p>
+            <div class="col-lg-6">
+                <div class="text-center pb-3">
+                    <h5 class="section-top-head">Complete</h5>
+                    <h2 class="section-head">Student Cycle</h2>
                 </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="text-center">
-                    <img src="assets/images/erp/attendance.png" alt="">
-                    <p>Attendance</p>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="text-center">
-                    <img src="assets/images/erp/grade-book.png" alt="">
-                    <p>Grade Book</p>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="text-center">
-                    <img src="assets/images/erp/result-processing.png" alt="">
-                    <p>Result Processing</p>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="text-center">
-                    <img src="assets/images/erp/discipline.png" alt="">
-                    <p>Discipline</p>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="text-center">
-                    <img src="assets/images/erp/nursing.png" alt="">
-                    <p>Nursing</p>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="text-center">
-                    <img src="assets/images/erp/certificate.png" alt="">
-                    <p>Certificate</p>
+                <div class="row gy-lg-5">
+                    <div class="col-lg-3 col-6">
+                        <div class="text-center">
+                            <img class="student-cycle-icon" src="assets/images/erp/addmission.png" alt="">
+                            <p>Admission</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="text-center">
+                            <img class="student-cycle-icon" src="assets/images/erp/id-card.png" alt="">
+                            <p>ID Card</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="text-center">
+                            <img class="student-cycle-icon" src="assets/images/erp/attendance.png" alt="">
+                            <p>Attendance</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="text-center">
+                            <img class="student-cycle-icon" src="assets/images/erp/grade-book.png" alt="">
+                            <p>Grade Book</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="text-center">
+                            <img class="student-cycle-icon" src="assets/images/erp/result-processing.png" alt="">
+                            <p>Result Processing</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="text-center">
+                            <img class="student-cycle-icon" src="assets/images/erp/discipline.png" alt="">
+                            <p>Discipline</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="text-center">
+                            <img class="student-cycle-icon" src="assets/images/erp/nursing.png" alt="">
+                            <p>Nursing</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="text-center">
+                            <img class="student-cycle-icon" src="assets/images/erp/certificate.png" alt="">
+                            <p>Certificate</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
+
+<section>
+    <div class="container">
+        <div class="life-cycle">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="life-cycle-row">
+                        <div class="row gy-5">
+                            <div class="col-lg-6">
+                                <div class="text-center">
+                                    <h3>15</h3>
+                                    <p>Years</p>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6">
+                                <div class="text-center">
+                                    <h3>3500</h3>
+                                    <p>Schools</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="text-center">
+                                    <h3>1000000</h3>
+                                    <p>Students</p>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6">
+                                <div class="text-center">
+                                    <h3>25</h3>
+                                    <p>TB Data</p>
+                                </div>
+                            </div>
+                        </div>
+                      
+                    </div>
+                </div>
+                <div class="col-lg-6 d-flex align-items-center">
+                    <div class="right text-white">
+                        <ul>
+                            <li>
+                                <h6>Configure Your own addmission Processes</h6>
+                                <p>Direct.Short LIst.Merit</p>
+                            </li>
+                            <li>
+                                <h6>Configure Your own addmission Processes</h6>
+                                <p>Direct.Short LIst.Merit</p>
+                            </li>
+                            <li>
+                                <h6>Configure Your own addmission Processes</h6>
+                                <p>Direct.Short LIst.Merit</p>
+                            </li>
+                            <li>
+                                <h6>Configure Your own addmission Processes</h6>
+                                <p>Direct.Short LIst.Merit</p>
+                            </li>
+                            <li>
+                                <h6>Configure Your own addmission Processes</h6>
+                                <p>Direct.Short LIst.Merit</p>
+                            </li>
+                            <li>
+                                <h6>Configure Your own addmission Processes</h6>
+                                <p>Direct.Short LIst.Merit</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="text-center pb-5">
+            <h5 class="section-top-head">Accurate</h5>
+            <h2 class="section-head">Finance Administration</h2>
+        </div>
+        <div class="text-center">
+            <img class="img-fluid" src="assets/images/erp/finance.png" alt="">
+        </div>
+    </div>
+</section>
+
+
 
 <?php include 'partials/footer.php';?>
